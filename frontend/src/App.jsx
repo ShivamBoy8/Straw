db5f54bed63b83464eab91223804e192726dbdd6
its-yoursk <shivanshmishra.786lucknow@gmail.com>
2026-07-21 12:00:40 +0530

    craeted navbar and productcard

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar/Navbar";
+import ProductCard from "./components/ProductCard/ProductCard";
+
+function App() {
+  return (
+    <>
+      
+      <ProductCard />
+    </>
+  );
+}
+
+export default App;
