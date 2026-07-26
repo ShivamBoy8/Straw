0ca91733e6102eefa6e87f87fa81f35abf2e6312
ShivamBoy8 <artistshivam3@gmail.com>
2026-07-26 20:04:39 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,22 +2,28 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
 
+require('dotenv').config()
 const authRoutes = require("./routes/authRoutes");
-const productRoutes=require("./routes/productRoutes")
+const productRoutes = require("./routes/productRoutes");
 const cartRoutes = require("./routes/cartRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const adminOrderRoutes = require("./routes/adminOrderRoutes");
 const aiRoutes = require("./routes/aiRoutes");
 const cors = require("cors");
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  process.env.CLIENT_URL,
+];
+
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: allowedOrigins,
     credentials: true,
-  })
+  }),
 );
 app.use(express.json());
-app.use(cookieParser())
-
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
